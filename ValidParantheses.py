275b3a76dea205a7efbb8f13f7ee41b33f855282
Raghav <raghav.kanu26@gmail.com>
2022-02-07 15:39:35 +0530

    Valid Parantheses:LeetCode

---
+++ ValidParantheses.py
@@ -0,0 +1,25 @@
+#Link : https://leetcode.com/problems/valid-parentheses/
+
+#This problem is based upon leetcode valid parentheses.
+
+def valid(s:str):
+    stack=[]
+    for i in s:
+        if i in ['[','{','(']:
+            stack.append(i)
+        if i == ')':
+            if stack.pop()!='(':
+                return False
+        if i == ']':
+            if stack.pop()!='[':
+                return False
+        if i == '}':
+            if stack.pop()!='{':
+                return False
+    if len(stack)>0:
+        return False
+    return True
+
+ab='()[][{}]'
+output=valid(ab)
+print(f"Valid Parantheses:{output}")
